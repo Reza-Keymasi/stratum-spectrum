0857a2af1863af66375ad3d7959da3a59d28740a
Reza-Keymasi <rezakeymasi1996@gmail.com>
2026-09-19 08:10:14 +0330

    feat: implement atomic transactions for habit creation process

--- src/app/api/habits/route.ts
+++ src/app/api/habits/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import mongoose from "mongoose";
 
 import connectToDB from "@/app/lib/db/connectToDB";
 import { CreateHabitSchema } from "@/features/habits/types/habit.schema";
@@ -19,26 +20,42 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
+  await connectToDB();
+
+  const session = await mongoose.startSession();
+
   try {
-    await connectToDB();
     const body = await req.json();
-
     const parsedBody = CreateHabitSchema.parse(body);
-    const habit = await Habit.create(parsedBody);
-    const initialEntryData = buildInitialEntryData(habit);
 
-    await HabitEntry.create({
-      ...initialEntryData,
-      habitId: habit._id,
-      weekNumber: 1,
-      frequencyType: habit.frequency.frequencyType,
-      completed: false,
-      completionRate: 0,
+    let habit;
+
+    await session.withTransaction(async () => {
+      const createdHabits = await Habit.create([parsedBody], { session });
+      habit = createdHabits[0];
+
+      const initialEntryData = buildInitialEntryData(habit);
+
+      await HabitEntry.create(
+        [
+          {
+            ...initialEntryData,
+            habitId: habit._id,
+            weekNumber: 1,
+            frequencyType: habit.frequency.frequencyType,
+            completed: false,
+            completionRate: 0,
+          },
+        ],
+        { session },
+      );
     });
 
     return NextResponse.json(habit, { status: 201 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: error }, { status: 500 });
+  } finally {
+    session.endSession();
   }
 }
