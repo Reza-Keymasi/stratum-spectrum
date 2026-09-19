f1e6d0a727c804843fb13a087a109dd79e4246cd
Reza-Keymasi <rezakeymasi1996@gmail.com>
2026-09-19 09:50:40 +0330

    feat: implement delete habit api route

--- src/app/api/habits/[id]/route.ts
+++ src/app/api/habits/[id]/route.ts
@@ -1,8 +1,10 @@
 import { NextResponse } from "next/server";
+import mongoose from "mongoose";
 
 import connectToDB from "@/app/lib/db/connectToDB";
 import Habit from "@/shared/lib/models/habit.model";
 import { ToggleHabitCompletionSchema } from "@/features/habits/types/habit.schema";
+import HabitEntry from "@/shared/lib/models/habit-entry.model";
 
 export async function GET(
   _: Request,
@@ -15,6 +17,39 @@ export async function GET(
     return NextResponse.json(habit);
   } catch (error) {
     return NextResponse.json(error);
+  }
+}
+
+export async function DELETE(
+  _: Request,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  await connectToDB();
+
+  const session = await mongoose.startSession();
+  try {
+    const { id: habitId } = await params;
+
+    let deletedHabit;
+
+    await session.withTransaction(async () => {
+      deletedHabit = await Habit.findByIdAndDelete(habitId, {
+        session,
+      });
+
+      if (!deletedHabit) throw new Error("Habit not found");
+
+      await HabitEntry.deleteMany({ habitId }, { session });
+    });
+
+    return NextResponse.json(
+      { message: "Habit deleted successfully" },
+      { status: 200 },
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error }, { status: 500 });
+  } finally {
+    session.endSession();
   }
 }
 
